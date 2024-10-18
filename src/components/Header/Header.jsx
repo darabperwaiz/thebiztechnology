f4eaef8de6cc9f8a2d649a127f1ae9bc5aaa50ec
darabperwaiz <darabperwaiz87@gmail.com>
2024-10-19 03:31:30 +0530

    some section updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import Logo from "./Logo/Logo"
 import Navbar from "./Navbar/Navbar"
 import style from './header.module.css'
 import { Outlet } from "react-router-dom"
+import Footer from "../Footer/Footer"
 // import { Wrapper } from "../../Reusable/Wrapper"
 
 const Header = () => {
@@ -13,6 +14,7 @@ const Header = () => {
       <Navbar />
     </header>
     <Outlet />
+    <Footer />
     </>
   )
 }

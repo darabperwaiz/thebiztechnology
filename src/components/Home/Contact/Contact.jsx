b15c5b9d58d1538642b974a3e4ac160a5a58801e
darabperwaiz <darabperwaiz87@gmail.com>
2024-10-20 02:26:50 +0530

    Fixed Contact Form

--- src/components/Home/Contact/Contact.jsx
+++ src/components/Home/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import style from "./contact.module.css";
 import { Button } from "../../../Reusable/Button";
 import { Link } from "react-router-dom";
 import { LuMail } from "react-icons/lu";
+import { FaArrowRightLong } from "react-icons/fa6"
 
 const Contact = () => {
   return (
@@ -15,8 +16,8 @@ const Contact = () => {
           </p>
         </div>
         <p className={style.des}>
-          Let me know your digitally need for your business and <span>we will contact
-          you within 6 hr.</span>
+          Let me know your digitally need for your business and{" "}
+          <span>we will contact you within 6 hr.</span>
         </p>
         <div className={style.midLeft}>
           <div className={style.rating}>
@@ -36,7 +37,31 @@ const Contact = () => {
           <Button text={"Get A Proposal"} />
         </div>
       </div>
-      <div className={style.right}>Contact Form....... Coming Soon!</div>
+      <div className={style.right}>
+        <div className={style.rightTop}>
+          <p>More About you</p>
+          <p>Fill out the contact form and we'll get in touch with you shortly.</p>
+        </div>
+        <form action="" className="scheduleForm">
+          <input className={style.input} type="text" placeholder="Your Name"/>
+          <input className={style.input} type="text" placeholder="Your Phone"/>
+          <select className={style.input} name="services" id="">
+            <option value="" selected disabled hidden>Select A Services</option>
+            <option value="seo">SEO</option>
+            <option value="smo">SMO</option>
+            <option value="gmb">GMB</option>
+            <option value="crm">CRM</option>
+            <option value="app development">App Development</option>
+            <option value="website development">Website Development</option>
+            <option value="link building">Link Building</option>
+            <option value="content writing">Content Writing</option>
+            <option value="sales funnel fixing">Sales Funnel Fixing</option>
+            <option value="website annual maintenance">Website Annual Maintenance</option>
+          </select>
+          <textarea className={style.input} name="" id="" placeholder="Your messages"></textarea>
+        </form>
+        <button className={style.button}>Schedule Now <span><FaArrowRightLong /></span></button>
+      </div>
     </div>
   );
 };

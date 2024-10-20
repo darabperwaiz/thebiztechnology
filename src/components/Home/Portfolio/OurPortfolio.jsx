fe282a23a285b2b41d118054295965b5a1fd6eeb
darabperwaiz <darabperwaiz87@gmail.com>
2024-10-21 02:32:02 +0530

    fixed portfolio

--- src/components/Home/Portfolio/OurPortfolio.jsx
+++ src/components/Home/Portfolio/OurPortfolio.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import style from './ourportfolio.module.css'
 import PrimaryButton from "../../../Reusable/PrimaryButton"
+import { portfolioData } from "./PortfolioData"
 
-const OurPortfolio = () => {
+const OurPortfolio = ({limit=6}) => {
   return (
     <div className={style.OurPortfolio}>
       <div className={style.top}>
@@ -18,7 +19,18 @@ const OurPortfolio = () => {
         </div>
       </div>
       <div className={style.mid}>
-        Projects.......... Coming Soon!
+        {portfolioData.map((t, i)=> i < 6 && (
+
+          <div className={`${style.gridItem} item${i}`} key={i}>
+            <img src={require(`../../../assets/images/porfolioimg/${t.img}`)} alt="" />
+          </div>
+        ))}
+        
+        {/* <div className={`${style.gridItem} item${'1'}`}>2</div>
+        <div className={`${style.gridItem} item${'2'}`}>3</div>
+        <div className={`${style.gridItem} item${'3'}`}>4</div>
+        <div className={`${style.gridItem} item${'4'}`}>5</div>
+        <div className={`${style.gridItem} item${'5'}`}>6</div> */}
       </div>
       <div className={style.bottom}>
         <PrimaryButton text={"View our all projects"}/>
---
+++ src/components/Home/Portfolio/PortfolioData.js
@@ -0,0 +1,63 @@
+export const portfolioData = [
+    {
+        id: 1,
+        category: 'website design',
+        img: 'project1.png'
+    },
+    {
+        id: 2,
+        category: 'mobile app',
+        img: 'project2.png'
+    },
+    {
+        id: 3,
+        category: 'seo',
+        img: 'project3.png'
+    },
+    {
+        id: 4,
+        category: 'ui',
+        img: 'project4.png'
+    },
+    {
+        id: 5,
+        category: 'website design',
+        img: 'project5.png'
+    },
+    {
+        id: 6,
+        category: 'mobile app',
+        img: 'project6.png'
+    },
+    {
+        id: 7,
+        category: 'seo',
+        img: 'project1.png'
+    },
+    {
+        id: 8,
+        category: 'ui',
+        img: 'project2.png',
+    },
+    {
+        id: 9,
+        category: 'website design',
+        img: 'project3.png'
+    },
+    {
+        id: 10,
+        category: 'mobile app',
+        img: 'project4.png'
+    },
+    {
+        id: 11,
+        category: 'seo',
+        img: 'project5.png'
+    },
+    {
+        id: 12,
+        category: 'ui',
+        img: 'project6.png'
+    },
+    
+]

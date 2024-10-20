1e7dc3d72deb2341dbcadd88ec49148168fffb66
darabperwaiz <darabperwaiz87@gmail.com>
2024-10-21 00:59:38 +0530

    fixed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,14 @@ import style from "./footer.module.css";
 import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { MdOutlineLocationOn } from "react-icons/md";
+import { BsTelephone } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <section className={style.footer}>
       <div className="container">
+        <div>
         <div className={style.footerWrapper}>
           <div className={style.left}>
             <div className={style.logo}>
@@ -62,14 +65,34 @@ const Footer = () => {
           <div className={style.quickLinks}>
             <p>Our Offices</p>
             <div className={style.links}>
-              <Link to="#">Site Map</Link>
-              <Link to="#">Terms and conditions</Link>
-              <Link to="#">Privacy Policy</Link>
-              <Link to="#">Refund Policy</Link>
-              <Link to="#">FAQ</Link>
-              <Link to="#">Portfolio</Link>
+              <div className={style.addressWrapper}>
+                <div className={style.address}>
+                  <p>USA</p>
+                  <div className={style.location}>
+                    <p><MdOutlineLocationOn style={{color: '#5840FF', fontSize: '16px'}}/> 27 Division St, New York, NY</p>
+                    <p><BsTelephone style={{color: '#5840FF', fontSize: '16px'}}/> +1(190) 122-8293</p>
+                  </div>
+                </div>
+                <div className={style.address}>
+                  <p>India</p>
+                  <div className={style.location}>
+                    <p><MdOutlineLocationOn style={{color: '#5840FF', fontSize: '16px'}} /> 27 Division St, New York, NY</p>
+                    <p><BsTelephone style={{color: '#5840FF', fontSize: '16px'}}/> +1(190) 122-8293</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={style.pGetway}>
+              <img src={require(`../../assets/images/pGatewayIcons/paypal.svg`).default} alt="" />
+              <img src={require(`../../assets/images/pGatewayIcons/stripe.svg`).default} alt="" />
+              <img src={require(`../../assets/images/pGatewayIcons/visa.svg`).default} alt="" />
+              <img src={require(`../../assets/images/pGatewayIcons/mastercard.svg`).default} alt="" />
             </div>
           </div>
+        </div>
+        <div className={style.copyright}>
+          <span>&#169;</span>Copyright TheBiztechnology. All right reserved
+        </div>
         </div>
       </div>
     </section>

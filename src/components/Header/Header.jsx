d33b426373067b4d6b864e40f59a34ee46887975
darabperwaiz <darabperwaiz87@gmail.com>
2024-10-23 22:11:26 +0530

    fixed navbar hide and show on scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from "./Logo/Logo"
 import Navbar from "./Navbar/Navbar"
 import style from './header.module.css'
 import { Outlet } from "react-router-dom"
 import Footer from "../Footer/Footer"
-// import { Wrapper } from "../../Reusable/Wrapper"
+
 
 const Header = () => {
+  const [show, setShow] = useState(true)
+  const [activeClass, setActiveClass] = useState(style.active)
+
+  useEffect(() => {
+    let windowWidth = window.innerWidth
+    if(windowWidth<= 600) {
+      setActiveClass(style.top)
+    } else {
+      setActiveClass(style.active)
+    }
+
+    windowWidth = windowWidth.innerWidth
+    let previousScrollPosition = 0;
+    let currentScrollPosition = 0;
+
+    window.addEventListener('scroll', function (e) {
+
+      // Get the new Value
+      currentScrollPosition = window.scrollY;
+
+      //Subtract the two and conclude
+      if (previousScrollPosition - currentScrollPosition < 0) {
+        setShow(false);
+      } else if (previousScrollPosition - currentScrollPosition > 0) {
+        setShow(true);
+      }
+
+      // Update the previous value
+      previousScrollPosition = currentScrollPosition;
+    });
+  }, []);
+
   return (
     <>
-    <header>
+    <header className={show ? `${activeClass}` : null}>
       <div className={style.nav}>
       <Logo />
       <Navbar />
